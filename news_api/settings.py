4c5b37b316492845c167d19f113a208bb801062b
Ivan Omelchenko <zver2010-3@ya.ru>
2021-07-22 19:52:15 +0300

    Changed settings

--- news_api/settings.py
+++ news_api/settings.py
@@ -19,13 +19,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-95lk@%dkz31ylq0xkiwus6a#1tzsyi9%hv)!fac)ou+l@&!%bg'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DJANGO_PRODUCTION' not in environ
 
-ALLOWED_HOSTS = ['*']
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = environ['SECRET_KEY'] if DEBUG else \
+    'django-insecure-95lk@%dkz31ylq0xkiwus6a#1tzsyi9%hv)!fac)ou' \
+    '+l@&!%bg'
+
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', 'floating-retreat-00950.herokuapp.com']
 
 # Application definition
 
